25d7e1a07bccc1dbc40fcb21bca48517a742a1b4
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-14 18:10:50 -0300

    test: ensure SignUpController returns 400 if no nick is provided

--- src/presentation/controllers/Sign/signup/SignUpController.spec.ts
+++ src/presentation/controllers/Sign/signup/SignUpController.spec.ts
@@ -34,4 +34,20 @@ describe('SignUpController', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
+  it('should  return 400 if no name is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        influencer: false,
+        avatar: 'any_avatar',
+        inTheNight: 'any_inTheNight',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.body).toEqual(new MissingParamError('nick'));
+  });
 });
