2748860066243c2b41acb51c1a14820e55b96f55
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-16 21:01:24 -0300

    feat: ensure emailValidator returns true if validator returns true

--- src/shared/infra/validators/emailValidatorAdapter.ts
+++ src/shared/infra/validators/emailValidatorAdapter.ts
@@ -1,7 +1,8 @@
 import { IEmailValidator } from '@/shared/validation/protocols/IEmailValidator';
+import validator from 'validator';
 
 export class EmailValidatorAdapter implements IEmailValidator {
   public isValid(email: string): boolean {
-    return false;
+    return validator.isEmail(email);
   }
 }
