b231532e3b65c1e4fa952cffc1a78caad04c4494
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-18 18:31:39 -0300

    test: ensure DBcreateAccount throws if hasher throws

--- src/modules/Sign/data/usecases/CreateAccount/DBcreateAccount.spec.ts
+++ src/modules/Sign/data/usecases/CreateAccount/DBcreateAccount.spec.ts
@@ -27,7 +27,6 @@ const makeSut = (): SutTypes => {
 describe('DBcreateAccount', () => {
   it('should call Hasher with correct password', async () => {
     const { sut, hasherStub } = makeSut();
-
     const hashSpy = jest.spyOn(hasherStub, 'hash');
     const accountData = {
       name: 'valid_name',
@@ -38,5 +37,23 @@ describe('DBcreateAccount', () => {
     };
     await sut.create(accountData);
     expect(hashSpy).toHaveBeenCalledWith('valid_password');
+  });
+
+  it('should throw  if hasher throws', async () => {
+    const { sut, hasherStub } = makeSut();
+    jest
+      .spyOn(hasherStub, 'hash')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      nick: 'valid_nick',
+      isInfluencer: false,
+      password: 'valid_password',
+    };
+    const promise = sut.create(accountData);
+    expect(promise).rejects.toThrow();
   });
 });
