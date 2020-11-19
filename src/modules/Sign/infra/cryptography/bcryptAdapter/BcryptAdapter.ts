8c1bd1fc5b375472881d2740d349dd4d91aa7a38
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-19 15:01:52 -0300

    feat: ensure BcryptAdapter retuns a hash on success

--- src/modules/Sign/infra/cryptography/bcryptAdapter/BcryptAdapter.ts
+++ src/modules/Sign/infra/cryptography/bcryptAdapter/BcryptAdapter.ts
@@ -5,7 +5,7 @@ export class BcryptAdapter implements IHasher {
   constructor(public readonly salt: number) {}
 
   public async hash(value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt);
-    return new Promise(resolve => resolve(null));
+    const hash = await bcrypt.hash(value, this.salt);
+    return hash;
   }
 }
