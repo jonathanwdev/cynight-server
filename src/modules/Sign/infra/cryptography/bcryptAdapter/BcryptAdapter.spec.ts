39aab368e01cd37a1e72a97c3fa9aac418fec523
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-19 15:02:07 -0300

    test: ensure BcryptAdapter retuns a hash on success

--- src/modules/Sign/infra/cryptography/bcryptAdapter/BcryptAdapter.spec.ts
+++ src/modules/Sign/infra/cryptography/bcryptAdapter/BcryptAdapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './BcryptAdapter';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise(resolve => resolve('hashed_password'));
+  },
+}));
+
 describe('Bcrypt Adapter', () => {
   it('should  calls bcrypt with correct values', async () => {
     const salt = 12;
@@ -8,5 +14,12 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.hash('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+  });
+
+  it('should  return a hash on success', async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+    const hash = await sut.hash('any_value');
+    expect(hash).toBe('hashed_password');
   });
 });
