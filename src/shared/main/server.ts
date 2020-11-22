2d7d3678c74cb998c143457db981b311437c8287
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-22 16:35:08 -0300

    feat: add server file working with express

--- src/shared/main/server.ts
+++ src/shared/main/server.ts
@@ -1,1 +1,9 @@
 import '../infra/database';
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.listen(process.env.APP_PORT, () =>
+  console.log(`server running at: http://localhost:${process.env.APP_PORT}`),
+);
