49bc15c58b2a9b5d19adc8c5ca3cf66ae95a17be
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-22 18:20:18 -0300

    feat: ensure parse body as json

--- src/shared/main/server.ts
+++ src/shared/main/server.ts
@@ -1,8 +1,5 @@
+import app from './config/app';
 import '../infra/database';
-import 'dotenv/config';
-import express from 'express';
-
-const app = express();
 
 app.listen(process.env.APP_PORT, () =>
   console.log(`server running at: http://localhost:${process.env.APP_PORT}`),
