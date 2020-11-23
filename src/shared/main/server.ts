4052be7fca96789d511896a79349880daa82921b
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-23 20:38:30 -0300

    refactor: move create connection in server.ts to pgHelper

--- src/shared/main/server.ts
+++ src/shared/main/server.ts
@@ -1,6 +1,12 @@
+import DBconnection from '../infra/database/helpers/PgHelper';
 import app from './config/app';
-import '../infra/database';
 
-app.listen(process.env.APP_PORT, () =>
-  console.log(`server running at: http://localhost:${process.env.APP_PORT}`),
-);
+DBconnection.create()
+  .then(() => {
+    app.listen(process.env.APP_PORT, () =>
+      console.log(
+        `server running at: http://localhost:${process.env.APP_PORT}`,
+      ),
+    );
+  })
+  .catch(console.error);
