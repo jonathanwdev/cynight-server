2a823819c5ca95e4e128abbac7a3224204c4ff9d
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-23 17:55:53 -0300

    test: ensure signup route returns an account on success

---
+++ src/shared/main/routes/signup.routes.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import app from '../config/app';
+
+describe('SignUp Routes', () => {
+  it('should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Joselito',
+        email: 'joselito@mail.com',
+        nick: 'jos107',
+        isInfluencer: false,
+        password: '123456',
+        passwordConfirmation: '123456',
+      })
+      .expect(200);
+  });
+});
