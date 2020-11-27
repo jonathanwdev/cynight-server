e8e2bcc23bb24664a9f12cc0c67a5cc06b99abfd
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-26 21:54:30 -0300

    feat: ensure signUpRoute returns 200 user on success

--- src/shared/main/routes/signUp.routes.ts
+++ src/shared/main/routes/signUp.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
+import { makeSignUpController } from '../../../modules/Sign/main/factories/controllers/SignupFactory';
+import { adaptRoute } from '../adapters/expressRouteAdapter';
 
 export default (router: Router): void => {
-  router.post('/signup', (req, res) => {
-    res.json({ ok: true });
-  });
+  router.post('/signup', adaptRoute(makeSignUpController()));
 };
