884585caf6a352571fc2de1cde22b74d2d78dac2
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-26 21:53:20 -0300

    feat: add mongoConnection to the server and separate app to another file

--- src/shared/main/config/app.ts
+++ src/shared/main/config/app.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
+
 import setUpMiddlewares from './middlewares';
 import setUpRoutes from './routes';
 
 const app = express();
 setUpMiddlewares(app);
 setUpRoutes(app);
+
 export default app;
--- src/shared/main/server.ts
+++ src/shared/main/server.ts
@@ -1,12 +1,10 @@
-import DBconnection from '../infra/database/helpers/PgHelper';
-import app from './config/app';
+import ConnectionHelper from '../infra/database/helpers/PgHelper';
 
-DBconnection.create()
-  .then(() => {
+ConnectionHelper.create()
+  .then(async () => {
+    const app = (await import('./config/app')).default;
     app.listen(process.env.APP_PORT, () =>
-      console.log(
-        `server running at: http://localhost:${process.env.APP_PORT}`,
-      ),
+      console.log(`Running at ${process.env.APP_PORT}`),
     );
   })
   .catch(console.error);
