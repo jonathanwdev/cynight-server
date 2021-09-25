4afcb6a41de908470f1cc89aad25f322dcfb29be
Jonathan Souza <jonathan.wdev@gmail.com>
2021-09-25 18:30:19 -0300

    feat: add new graphql queries and mutations

--- src/GraphQL/modules/FriendShip/resolvers/FriendShipResolver.ts
+++ src/GraphQL/modules/FriendShip/resolvers/FriendShipResolver.ts
@@ -1,8 +1,17 @@
 import { isAuthenticated, MyContext } from '@GraphQL/middleware/Auth';
 import { isValidUser } from '@GraphQL/middleware/User';
+import DeleteFriendShipServiceFactory from '@Modules/FriendShip/factories/DeleteFriendShipServiceFactory';
+import FindAllBlockedFriendsServiceFactory from '@Modules/FriendShip/factories/FindAllBlockedFriendsServiceFactory';
 import FindAllFriendsServiceFactory from '@Modules/FriendShip/factories/FindAllFriendsServiceFactory';
 import Friends from '@Typeorm/entity/Friends';
-import { Resolver, Query, UseMiddleware, Ctx } from 'type-graphql';
+import {
+  Resolver,
+  Query,
+  UseMiddleware,
+  Arg,
+  Ctx,
+  Mutation,
+} from 'type-graphql';
 
 @Resolver()
 export class FriendShipResolver {
@@ -13,6 +22,30 @@ export class FriendShipResolver {
   ): Promise<Friends[] | undefined> {
     const { userId } = ctx.req!;
     const friends = await FindAllFriendsServiceFactory().run(userId);
+    return friends;
+  }
+
+  @UseMiddleware(isAuthenticated, isValidUser)
+  @Query(() => [Friends], { nullable: true })
+  public async FindAllBlockedFriendShips(
+    @Ctx() ctx: MyContext,
+  ): Promise<Friends[] | undefined> {
+    const { userId } = ctx.req!;
+    const friends = await FindAllBlockedFriendsServiceFactory().run(userId);
+    return friends;
+  }
+
+  @UseMiddleware(isAuthenticated, isValidUser)
+  @Mutation(() => Boolean, { nullable: true })
+  public async DeleteFriendShip(
+    @Arg('friendId', () => String) friendId: string,
+    @Ctx() ctx: MyContext,
+  ): Promise<boolean> {
+    const { userId } = ctx.req!;
+    const friends = await DeleteFriendShipServiceFactory().run({
+      friendId,
+      userId,
+    });
     return friends;
   }
 }
